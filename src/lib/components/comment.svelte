9a8ea686de09e111be03347d489331b625b5cc0a
the-byte-bender <cccefg2@gmail.com>
2024-07-17 04:22:13 +0300

    feat: Split comments to comment component and comment list component

---
+++ src/lib/components/comment.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import { enhance } from "$app/forms";
+  import type { ClientsideComment, ClientsideUser } from "$lib/types";
+  import Modal from "./modal.svelte";
+  export let comment: ClientsideComment;
+  export let user: ClientsideUser | undefined = undefined;
+  export let isAdmin: boolean = false;
+  let isDeletionModalVisible: boolean = false;
+</script>
+
+<div class="comment">
+  <h3>
+    <a href={`/user/${comment.user.id}`}>{comment.user.displayName}</a>
+  </h3>
+  <pre>{comment.content}</pre>
+  {#if isAdmin || (user && user.id === comment.user.id)}
+    <div id="comment-actions">
+      <button on:click={() => (isDeletionModalVisible = true)}>Delete</button>
+      <Modal bind:visible={isDeletionModalVisible}>
+        <h2>Delete this comment?</h2>
+        <p>Are you sure? This action cannot be undone.</p>
+        <p>Comment content:</p>
+        <pre>{comment.content}</pre>
+        <button on:click={() => (isDeletionModalVisible = false)}>Cancel</button
+        >
+        <form use:enhance action="?/delete_comment" method="post">
+          <input type="hidden" name="id" value={comment.id} />
+          <button type="submit">Confirm delete</button>
+        </form>
+      </Modal>
+    </div>
+  {/if}
+</div>
+
+<style>
+  .comment {
+    margin-top: 1rem;
+    padding: 0.5rem;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .comment h3 a {
+    color: #007bff;
+    text-decoration: none;
+  }
+
+  .comment pre {
+    white-space: pre-wrap;
+    background-color: #fff;
+    padding: 0.5rem;
+    border: none;
+    margin-top: 0.5rem;
+  }
+
+  .comment #comment-actions {
+    margin-top: 0.5rem;
+  }
+</style>
--- src/lib/components/comment_list.svelte
+++ src/lib/components/comment_list.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import { enhance } from "$app/forms";
+  import Comment from "./comment.svelte";
   import type { ClientsideComment, ClientsideUser } from "$lib/types";
 
   export let comments: ClientsideComment[];
-  export let user: ClientsideUser|undefined = undefined;
+  export let user: ClientsideUser | undefined = undefined;
   export let isAdmin: boolean = false;
 </script>
 
@@ -11,20 +11,7 @@
   <h2>Comments</h2>
   {#if comments.length > 0}
     {#each comments as comment (comment.id)}
-      <div>
-        <h3>
-          <a href={`/user/${comment.user.id}`}>{comment.user.displayName}</a>
-        </h3>
-        <pre>{comment.content}</pre>
-        {#if isAdmin || (user && user.id === comment.user.id)}
-          <div id="comment-actions">
-            <form use:enhance action="?/delete_comment" method="post">
-              <input type="hidden" name="id" value={comment.id} />
-              <button type="submit">Delete</button>
-            </form>
-          </div>
-        {/if}
-      </div>
+      <Comment {comment} {user} {isAdmin} />
     {/each}
   {:else}
     <p>No comments yet</p>
@@ -44,31 +31,6 @@
     color: #333;
     font-size: 1.5rem;
     margin-bottom: 1rem;
-  }
-
-  section[role="group"] div {
-    margin-top: 1rem;
-    padding: 0.5rem;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
-
-  section[role="group"] div h3 a {
-    color: #007bff;
-    text-decoration: none;
-  }
-
-  section[role="group"] div pre {
-    white-space: pre-wrap;
-    background-color: #fff;
-    padding: 0.5rem;
-    border: none;
-    margin-top: 0.5rem;
-  }
-
-  section[role="group"] div #comment-actions {
-    margin-top: 0.5rem;
   }
 
   section[role="group"] p {
