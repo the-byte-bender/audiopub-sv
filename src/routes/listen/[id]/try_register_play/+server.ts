952dd70a73aa2875c2896544b6bff4d304250994
the-byte-bender <cccefg2@gmail.com>
2024-07-26 19:16:16 +0300

    feat: Update try_register_play server code to use client IP for play registration

--- src/routes/listen/[id]/try_register_play/+server.ts
+++ src/routes/listen/[id]/try_register_play/+server.ts
@@ -21,14 +21,14 @@ import { error, json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
 export const POST: RequestHandler = async (event) => {
-  const user = event.locals.user;
+  const ip = event.request.headers.get("x-forwarded-for");
   const audio = await Audio.findByPk(event.params.id);
-  if (!user) {
+  if (!ip) {
     return error(401, "Unauthorized");
   }
   if (!audio) {
     return error(404, "Audio not found");
   }
-  const result = await audio.registerPlay(user.id);
+  const result = await audio.registerPlay(ip);
   return json(result);
 };
